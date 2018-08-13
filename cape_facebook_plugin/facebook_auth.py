6e5b3baf480916284cec9c597f34db3df2b8d4f8
Mike Sheldon <mike@bloomsbury.ai>
2018-08-13 14:42:18 +0100

    Add license headers

--- cape_facebook_plugin/facebook_auth.py
+++ cape_facebook_plugin/facebook_auth.py
@@ -1,3 +1,17 @@
+# Copyright 2018 BLEMUNDSBURY AI LIMITED
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from cape_facebook_plugin.facebook_settings import URL_BASE
 from cape_facebook_plugin.facebook_settings import facebook_auth_endpoints
 from userdb.facebook_bot import FacebookBot
