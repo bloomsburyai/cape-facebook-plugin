a1117b676587e1bb2cdf81aa9b5b69657a6a2e46
Mike Sheldon <mike@bloomsbury.ai>
2018-08-13 15:09:37 +0100

    Remove unused variable

--- cape_facebook_plugin/facebook_settings.py
+++ cape_facebook_plugin/facebook_settings.py
@@ -20,4 +20,3 @@ facebook_event_endpoints = Blueprint('facebook_event_endpoints')
 facebook_auth_endpoints = Blueprint('facebook_auth_endpoints')
 facebook_verification = os.getenv('CAPE_FACEBOOK_VERIFICATION', 'REPLACEME')
 
-THIS_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__)))
